274c90924b3da4e636267abf7df26671df7c71f1
woorim02 <99327851+woorim02@users.noreply.github.com>
2024-07-07 02:52:10 +0900

    경품이벤트 목록 추가

--- vite/src/routes/MainRoutes.jsx
+++ vite/src/routes/MainRoutes.jsx
@@ -2,9 +2,9 @@ import { lazy } from 'react';
 
 // project imports
 import MainLayout from 'layout/MainLayout';
-import Loadable from 'ui-component/Loadable';
 import Home from 'views/Home';
 import CgvEventPage from 'views/cgv/event/EventPage';
+import CgvGiveawayPage from 'views/cgv/event/giveaway/GiveawayPage';
 
 
 
@@ -19,13 +19,12 @@ const MainRoutes = {
       element: <Home />
     },
     {
-      path: 'cgv',
-      children:[
-        {
-          path: 'event',
-          element: <CgvEventPage/>
-        }
-      ]
+      path: 'cgv/event',
+      element: <CgvEventPage/>
+    },
+    {
+      path: 'cgv/event/giveaway',
+      element: <CgvGiveawayPage/>,
     }
   ]
 };
